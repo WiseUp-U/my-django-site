f4918751e74cd969f6d3f35b1c425c946ba9066e
WiseUp-U <nature1022005@gamil.com>
2026-03-17 15:21:24 +0630

    fixeds

--- shop/settings.py
+++ shop/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-LOCALE_PATHS = [os.payh.join(BASE_DIR, 'locale')]
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 
 # Quick-start development settings - unsuitable for production
