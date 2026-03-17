f9038d0189a017ebea8470ade47c33998f232de1
WiseUp-U <nature1022005@gmail.com>
2026-03-17 16:36:05 +0630

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIS_URL = "/media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
